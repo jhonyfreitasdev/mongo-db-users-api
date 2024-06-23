529e251de2afa9e88104c7c13098dcba7f5ed186
Jhony Freitas <jhony00._@hotmail.com>
2024-06-23 17:27:37 -0300

    Criando rotas para criação de usuários

---
+++ server.js
@@ -0,0 +1,67 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const app = express();
+app.use(express.json());
+const prisma = new PrismaClient();
+
+app.post('/users', async (req, res) => {
+  const {name, age, email} = req.body
+
+  await prisma.user.create({
+    data: {
+      age: age,
+      name: name,
+      email: email,
+    }
+  })
+
+  res.status(201).send(req.body);
+});
+
+app.get('/users', async (req, res) => {
+  const params = req.query;
+
+  const where = Object.keys(params).reduce((acc, key) => {
+    acc[key] = key === 'name'
+     ? {equals : params[key], mode: 'insensitive'}
+     : params[key];
+    return acc;
+  }, {});
+
+  const users = await prisma.user.findMany({
+    where
+  });
+  res.status(200).json(users);
+});
+
+app.put('/users/:id', async (req, res) => {
+  const id = req.params.id;
+  const data = req.body
+
+  await prisma.user.update({
+    where: {
+      id
+    },
+    data
+  })
+
+  res.status(200).send(req.body);
+});
+
+app.delete('/users/:id', async (req, res) => {
+  const id = req.params.id;
+
+  await prisma.user.delete({
+    where: {
+      id
+    },
+  })
+
+  res.status(200).json({message: 'Usuário excluido com sucesso'});
+});
+
+
+app.listen(3003, () => {
+  console.log('server running on port http://localhost:3003');
+});
